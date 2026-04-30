cf5f698e909de52ec4bc80e71416ab6a153241cf
Dhiraj Kumar <selenophile3582@gmail.com>
2026-04-30 19:24:32 +0530

    Day 01 : Add Basic Hello World Program

---
+++ Day01_Hello.java
@@ -0,0 +1,9 @@
+// Day 01 : Basic Hello World Program 
+
+
+public class Day01_Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World! ");
+        System.out.println("This is day one of my 30 days Java Challenge !");
+    }
+}
