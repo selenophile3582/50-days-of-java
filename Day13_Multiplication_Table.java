ab90e57cfb7175816638ae4912cb4f09096eef96
Dhiraj Kumar <selenophile3582@gmail.com>
2026-05-12 08:04:30 +0530

    Day 13 : Add Multiplication Table Program

---
+++ Day13_Multiplication_Table.java
@@ -0,0 +1,21 @@
+//Printing Multiplication Table
+
+import java.util.Scanner;
+
+public class Day13_Multiplication_Table {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number : ");
+        int n = sc.nextInt();
+        System.out.println("Multiplication Table of "+n );
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("%d X %d = %d \n", n, i, n * i);
+        }
+
+        sc.close();
+    }
+
+}
